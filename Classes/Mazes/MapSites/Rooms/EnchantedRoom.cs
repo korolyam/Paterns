dda363686a9f55220852e9614ab1e44db8c53fde
Korolyam <worldofcrew1@gmail.com>
2025-10-31 09:25:34 +0200

    The slightest adjustment

--- Classes/Mazes/MapSites/Rooms/EnchantedRoom.cs
+++ Classes/Mazes/MapSites/Rooms/EnchantedRoom.cs
@@ -8,9 +8,16 @@ namespace Classes.Mazes.MapSites.Rooms
 {
     public class EnchantedRoom : Room
     {
+        private Spell spell;
+
         public EnchantedRoom(int noOfRoom, Spell spell) : base(noOfRoom)
         {
-            
+            this.spell = spell;    
+        }
+
+        public void Enter(Direction direction)
+        {
+            this.Sides[Direction.East].Enter();
         }
     }
 }
